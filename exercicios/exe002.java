632e177a8633aa487526997f67eac68476475fd6
Cassio Potter <cassio.potter.barbosa.dev@gmail.com>
2023-02-07 23:16:43 -0300

    Create exe002.java
    respondendo ao usuario

---
+++ exercicios/exe002.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class exe002 {
+    public static void main(String[]args) {
+        Scanner sc = new Scanner (System.in);
+        String nome;
+
+        System.out.println("Ola qual é o seu nome? ");
+        nome=sc.next();
+        System.out.println("Ola" + nome + "seja muito bem vindo!");
+
+    }
+    
+}
