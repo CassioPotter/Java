f1a487bdc0311fc22c73b5bc9510d7039427b319
Cassio Potter <cassio.potter.barbosa.dev@gmail.com>
2023-02-14 19:52:52 -0300

    Create aula025.java

---
+++ aulas/aula025.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class aula025 {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner (System.in);
+        String nome;
+        int inteiro;
+        double flutuante;
+        char letra;
+
+
+
+        System.out.println("Nome:");
+        nome = sc.next();
+
+        System.out.println("Numero inteiro:");
+        inteiro =sc.nextInt();
+
+        System.out.println("Numero flutuante;");
+        flutuante = sc.nextDouble();
+
+        System.out.println("Letra:");
+        letra = sc.next().charAt(0);
+
+        System.out.println(nome);
+        System.out.println(inteiro);
+        System.out.println(flutuante);
+        System.out.println(letra);
+
+
+
+        
+        
+    }
+
+
+
+    
+}
