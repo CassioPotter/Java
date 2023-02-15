22c3bd348d636ac6af218fb373e8ea9f4b800417
Cassio Potter <cassio.potter.barbosa.dev@gmail.com>
2023-02-15 20:19:01 -0300

    Create aula26_lendo_ate_quebra_de_linha.java

---
+++ aulas/aula26_lendo_ate_quebra_de_linha.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class aula26_lendo_ate_quebra_de_linha {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        String frase;
+
+        System.out.println("Digite seu nome completo:");
+        frase = sc.nextLine();
+
+        System.out.printf("Ola %s seja bem vindo",frase);
+
+
+
+
+
+
+
+
+    }
+    
+}
