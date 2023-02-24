142abae1de483e0bf76e23d845a866f382e4b108
Cassio Potter <cassio.potter.barbosa.dev@gmail.com>
2023-02-23 21:56:24 -0300

    Create aula049_for.java

---
+++ aulas/aula049_for.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class aula049_for {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner ( System.in);
+
+        int soma=0;
+        int n=0;
+        int x=0;
+
+        System.out.println("Quantas vezes queres repetir a soma? ");
+        n = sc.nextInt();
+
+        for ( int i=0; i<n; i=i+1){
+            System.out.println("Digite um valor:");
+            x=sc.nextInt();
+            soma = soma + x;
+        }
+        System.out.println("Você digitou "+n+" valores e a sua soma deu "+soma);
+
+
+
+    }
+    
+}
