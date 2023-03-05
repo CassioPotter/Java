a86364a6054c16017282c6a9c92b4130f2a08eb4
Cassio Potter <cassio.potter.barbosa.dev@gmail.com>
2023-03-05 14:48:32 -0300

    Update aula088_vetores.java

--- aulas/aula088_vetores.java
+++ aulas/aula088_vetores.java
@@ -3,8 +3,10 @@ import java.util.Scanner;
 public class aula088_vetores {
     public static void main(String[] args) {
         Scanner sc = new Scanner (System.in);
+
         
 
+        sc.close()
     }
     
 }
