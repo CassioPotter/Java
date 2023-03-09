c78483cf7bc2c88ee14c52b05a9957464ddd0f81
Cassio Potter <cassio.potter.barbosa.dev@gmail.com>
2023-03-09 20:34:02 -0300

    Update aula088_vetores.java

--- aulas/aula088_vetores.java
+++ aulas/aula088_vetores.java
@@ -4,13 +4,16 @@ public class aula088_vetores {
     public static void main(String[] args) {
         Scanner sc = new Scanner (System.in);
 
+        double soma = 0.0;
         int n=sc.nextInt();
         double[] vect = new double[n];
 
         for (int i = 0; i<n; i++ ){
-            vect [i] = sc.nextDouble() // vai recebendo o que o usuario digitar criando um vetor de n lugares
+            vect [i] = sc.nextDouble(); // vai recebendo o que o usuario digitar criando um vetor de n lugares
+            soma += vect[i];
         }
 
+        System.out.println("A soma dos vetores deu "+ soma);
         sc.close();
     }
     
