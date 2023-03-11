2f01ede622fc5e86394dceab5675073db7d6ab79
Cassio Potter <cassio.potter.barbosa.dev@gmail.com>
2023-03-11 15:35:48 -0300

    Create aula097_listas.java

---
+++ aulas/aula097_listas.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+import java.util.List;
+
+public class aula097_listas {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+
+    }
+
+}
