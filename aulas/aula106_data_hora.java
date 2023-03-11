1dbb98767b91a9834c5ba39f66b383626526ee3f
Cassio Potter <cassio.potter.barbosa.dev@gmail.com>
2023-03-11 19:40:18 -0300

    Update aula106_data_hora.java

--- aulas/aula106_data_hora.java
+++ aulas/aula106_data_hora.java
@@ -1,5 +1,21 @@
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 public class aula106_data_hora {
     public static void main(String[] args) {
         
+        //instanciar data
+        //data local
+        LocalDate d01 = LocalDate.now();
+        System.out.println(d01);
+
+        //data e hora local
+        LocalDateTime d02 = LocalDateTime.now();
+        System.out.println(d02);
+
+        //data hora global gnt
+        Instant d03 = Instant.now();
+        sysout
     }
 }
