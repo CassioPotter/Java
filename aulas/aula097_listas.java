d3402191286adbd7677d7b7403ea4a98709ac59c
Cassio Potter <cassio.potter.barbosa.dev@gmail.com>
2023-03-11 16:08:33 -0300

    Update aula097_listas.java

--- aulas/aula097_listas.java
+++ aulas/aula097_listas.java
@@ -1,18 +1,23 @@
-import java.util.Scanner;
-import java.util.List;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 public class aula097_listas {
     public static void main(String[] args) {
         Scanner sc = new Scanner (System.in);
         
-        List<integer> list = new ArrayList<>();
+        List<Integer> list = new ArrayList<>();
 
-        list.add("1");
-        list.add("2");
-        list.add("3");
-        list.add(2,"9") // na posição 2 vai ser incrementado o numero 9
-        
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(2,9); // na posição 2 vai ser incrementado o numero 9
+
+        System.out.println("\n");
+        for (Integer x : list){
+            System.out.println(x);
+        }
+
 
 
     }
