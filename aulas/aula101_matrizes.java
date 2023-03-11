694c6a885ddbd8f5ea15d6cd1662195b039509c2
Cassio Potter <cassio.potter.barbosa.dev@gmail.com>
2023-03-11 17:56:23 -0300

    Create aula101_matrizes.java

---
+++ aulas/aula101_matrizes.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class aula101_matrizes {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        
+        int n = sc.nextInt();//quantidade de linha e coluna
+        int [][] mat = new int [n] [n]; //n vai entra nas matrizes
+
+        for (int x = 0; x < n; x++){
+            
+        }
+
+        sc.close();
+    }
+}
