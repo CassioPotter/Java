fb2ce047f056150d460e505a18359213eef5d150
Cassio Potter <cassio.potter.barbosa.dev@gmail.com>
2023-03-13 19:00:55 -0300

    Update aula_2.java

--- poo/aula_2.java
+++ poo/aula_2.java
@@ -3,6 +3,13 @@ package poo;
 public class aula_2 {
     public static void main(String[] args) {
         
+        caneta c1 = new caneta();
+
+        c1.cor = "azul";
+        c1.ponta = 0.5f;
+        c1.tampada = false;
+
+        
     }
     
 }
