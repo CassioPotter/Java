c44463cedd51e8cf6cf006125766c8ebde6c7eea
Cassio Potter <cassio.potter.barbosa.dev@gmail.com>
2023-03-14 19:32:30 -0300

    Create aula_03.java

---
+++ poo/aula_03.java
@@ -0,0 +1,13 @@
+public class aula_03 {
+    
+    public static void main(String[] args) {
+        
+        caneta03 c1 = new caneta03();
+        c1.modelo = "bic";
+        c1.cor = "vermelho";
+
+        c1.status();
+        c1.destampar();
+        c1.rabiscar();
+    }
+}
