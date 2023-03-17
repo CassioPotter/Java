99242880e9fd0cdc71b51a7e3b94c6077f76e23b
Cassio Potter <cassio.potter.barbosa.dev@gmail.com>
2023-03-17 00:29:40 -0300

    Create aula_04.java

---
+++ poo/aula_04.java
@@ -0,0 +1,11 @@
+public class aula_04 {
+    public static void main(String[] args) {
+        
+        caneta04 c1 = new caneta04();
+
+        c1.setModelo("bic");
+        c1.setPonta(0.5f);
+
+        c1.status();
+    }
+}
